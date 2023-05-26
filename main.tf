1d83f188aac1e6ce97e08dd2cd5d77d758a212bd
powellquiring <powellquiring@gmail.com>
2023-05-26 06:30:52 -0700

    just outputs

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
+/*
+original contents:
+
 module "landing-zone-vpc" {
   source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vpc?archive=tgz&flavor=standard&kind=terraform&name=deploy-arch-ibm-slz-vpc&version=v3.8.3"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = "us-south"
   prefix           = "slz-vpc"
 }
+*/
 
+output "prefix" {
+  value = var.prefix
+}
+
+output "region" {
+  value = var.region
+}
